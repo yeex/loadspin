0b6787fd4d17f5f0e820fec84481759a55c19bc1
Ntwari Moise <yeex@users.noreply.github.com>
2020-07-20 23:13:24 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  title = "D'Coders Angular Tutorials";
+  isLoading = false;
+
+  toggleLoading = () => {
+    this.isLoading = true;
+
+    //Faking an API call
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 3000);
+  };
+}
